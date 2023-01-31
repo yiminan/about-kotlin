441e4583bc1816543f2b4fe48faa7e055f82a51a
orgojy <giveawesome@gmail.com>
2023-01-31 23:25:47 +0900

    Add ListExample.kt to show example values

--- src/main/kotlin/ListExample.kt
+++ src/main/kotlin/ListExample.kt
@@ -1,11 +1,14 @@
 fun main() {
-    val a: List<Int?> = listOf(1, 2, null, 4)
-    val b: List<Int?> = listOf(1, 2, 3, 4)
+    val a: List<Int> = emptyList()
+    val b: List<Int> = listOf(1, 2, 3, 4)
 
-    val c: List<Int>? = null
-    val d: List<Int>? = listOf(1, 2, 3, 4)
+    val c: List<Int?> = listOf(1, 2, null, 4)
+    val d: List<Int?> = listOf(1, 2, 3, 4)
 
-    val e: List<Int?>? = null
-    val f: List<Int?>? = listOf(1, 2, 3, 4)
-    val g: List<Int?>? = listOf(1, 2, null, 4)
+    val e: List<Int>? = null
+    val f: List<Int>? = listOf(1, 2, 3, 4)
+
+    val g: List<Int?>? = null
+    val h: List<Int?>? = listOf(1, 2, 3, 4)
+    val i: List<Int?>? = listOf(1, 2, null, 4)
 }
