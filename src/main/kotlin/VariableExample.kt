e718d184626db2ef2793aa545568b0e107272246
orgojy <giveawesome@gmail.com>
2023-02-05 20:51:26 +0900

    Refactor VariableExample.kt to add example

--- src/main/kotlin/VariableExample.kt
+++ src/main/kotlin/VariableExample.kt
@@ -13,20 +13,16 @@ fun main() {
     var safeCallNumber: Long? = null
     safeCallNumber = 10L
 
-    val dollar = 10 // 기본이 Int
-    if (dollar is Int) {
+    if (10 is Int) {
         println("dollar is Int")
     }
-    val dollar2 = 10L
-    if (dollar2 is Long) {
+    if (10L is Long) {
         println("dollar2 is Long")
     }
-    val dollar3 = 10.0 // 기본이 Double
-    if (dollar3 is Double) {
+    if (10.0 is Double) {
         println("dollar3 is Double")
     }
-    val dollar4 = 10.0f
-    if (dollar4 is Float) {
+    if (10.0f is Float) {
         println("dollar4 is Float")
     }
 }
