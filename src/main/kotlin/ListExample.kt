4122eb7284e4019e3558dc11991b6f8041a715f7
orgojy <giveawesome@gmail.com>
2023-02-12 21:32:20 +0900

    Update ListExample.kt to build new list by immutable list

--- src/main/kotlin/ListExample.kt
+++ src/main/kotlin/ListExample.kt
@@ -15,5 +15,6 @@ fun main() {
     val h: List<Int?>? = listOf(1, 2, 3, 4)
     val i: List<Int?>? = listOf(1, 2, null, 4)
 
+//    val immutableList = a.add(5) // error
     val immutableList = a.plus(5) // create new list
 }
