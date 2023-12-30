d6b52e9631d1954392a5d2226ae46006225305a6
orgojy <giveawesome@gmail.com>
2023-12-30 23:57:12 +0900

    chore(build.gradle.kts): add dependency for kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2") // kotlin junit 처럼 쓸 수 있는 Spec 들이 정의 됨
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2") // shouldBe... etc 와같이 Assertions 의 기능을 제공
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2") // spring boot test 를 위해서 추가
 }
 
 tasks.test {
