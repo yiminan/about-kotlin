5582d0ebd702ac86079f0037f7ece8c4edc376c2
yiminan <giveawesome@gmail.com>
2023-12-31 00:28:41 +0900

    test(ListTest.kt): add test case to check list size

--- src/test/kotlin/kotest/ListTest.kt
+++ src/test/kotlin/kotest/ListTest.kt
@@ -1,11 +1,19 @@
 package kotest
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldHaveSize
 
 class ListTest : FunSpec({
     test("emptyList의 size는 0이다.") {
         val emptyList: List<Int> = emptyList()
+
         emptyList shouldHaveSize 0
+    }
+
+    test("listOf(1, 2, 3, 4)의 size는 4이다.") {
+        val list: List<Int> = listOf(1, 2, 3, 4)
+
+        list shouldHaveSize 4
     }
 })
